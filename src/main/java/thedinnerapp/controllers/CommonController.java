d59fd798a07667b59128546569007c8ff4c318aa
maryshb <mariajewula28@gmail.com>
2021-10-19 11:28:21 +0200

    Add Bootstrap to main page.

--- src/main/java/thedinnerapp/controllers/CommonController.java
+++ src/main/java/thedinnerapp/controllers/CommonController.java
@@ -6,12 +6,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import thedinnerapp.services.impl.RestaurantServiceImpl;
+import thedinnerapp.session.SessionObject;
+
+import javax.annotation.Resource;
 
 @Controller
 public class CommonController {
 
     @Autowired
     RestaurantServiceImpl restaurantService;
+
+    @Resource
+    SessionObject sessionObject;
 
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
@@ -20,6 +26,7 @@ public class CommonController {
     @RequestMapping(value = "/main", method = RequestMethod.GET)
     public String main(Model model) {
         model.addAttribute("restaurants", this.restaurantService.getAllRestaurants());
+        model.addAttribute("isLogged", this.sessionObject.isLogged());
         return "main";
     }
 }
