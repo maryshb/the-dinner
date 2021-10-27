25789fbf8e9ccb88926ae368b094e504b33b59fe
maryshb <mariajewula28@gmail.com>
2021-10-27 12:03:09 +0200

    Add mappings for contact, categories

--- src/main/java/thedinnerapp/controllers/CommonController.java
+++ src/main/java/thedinnerapp/controllers/CommonController.java
@@ -3,18 +3,21 @@ package thedinnerapp.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import thedinnerapp.model.Restaurant;
 import thedinnerapp.services.impl.RestaurantServiceImpl;
 import thedinnerapp.session.SessionObject;
 
 import javax.annotation.Resource;
+import java.util.Arrays;
 
 @Controller
 public class CommonController {
 
     @Autowired
     RestaurantServiceImpl restaurantService;
+
+
 
     @Resource
     SessionObject sessionObject;
@@ -26,7 +29,27 @@ public class CommonController {
     @RequestMapping(value = "/main", method = RequestMethod.GET)
     public String main(Model model) {
         model.addAttribute("restaurants", this.restaurantService.getAllRestaurants());
+        model.addAttribute("cuisines",Arrays.asList(Restaurant.Cuisine.values()));
         model.addAttribute("isLogged", this.sessionObject.isLogged());
+        model.addAttribute("role", this.sessionObject.isLogged() ? this.sessionObject.getLoggedUser().getRole().toString() : null);
+
+        System.out.println(Arrays.asList(Restaurant.Cuisine.values()));
         return "main";
     }
+
+
+    @RequestMapping(value = "/contact", method = RequestMethod.GET)
+    public String contact(Model model){
+        model.addAttribute("isLogged", this.sessionObject.isLogged());
+        return "contact";
+    }
+
+    @RequestMapping (path = "/kategorie")
+    public String greetingSubmit(@ModelAttribute Restaurant.Cuisine cuisine, Model model) {
+        model.addAttribute("cuisine", cuisine);
+        model.addAttribute("isLogged", this.sessionObject.isLogged());
+        return "contact";
+    }
+
+
 }
