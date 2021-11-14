f9f2f4bd6d94c84c3da27fb4b3e67909998ce7be
maryshb <mariajewula28@gmail.com>
2021-11-14 13:48:06 +0100

    Add user login and password tests

--- src/test/java/thedinnerapp/services/impl/UserServiceImplTest.java
+++ src/test/java/thedinnerapp/services/impl/UserServiceImplTest.java
@@ -1,6 +1,8 @@
 package thedinnerapp.services.impl;
+
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentMatcher;
 import org.mockito.ArgumentMatchers;
@@ -17,6 +19,9 @@ import thedinnerapp.dao.IRestaurantDAO;
 import thedinnerapp.dao.IUserDAO;
 import thedinnerapp.model.User;
 import thedinnerapp.model.view.RegistrationModel;
+import thedinnerapp.services.IBasketService;
+import thedinnerapp.services.IItemService;
+import thedinnerapp.services.IRestaurantService;
 import thedinnerapp.services.IUserService;
 import thedinnerapp.session.SessionObject;
 
@@ -30,6 +35,15 @@ public class UserServiceImplTest {
     @Autowired
     IUserService userService;
 
+    @Autowired
+    IBasketService basketService;
+
+    @Autowired
+    IRestaurantService restaurantService;
+
+    @Autowired
+    IItemService itemService;
+
     @MockBean
     IUserDAO userDAO;
 
@@ -39,24 +53,42 @@ public class UserServiceImplTest {
     @MockBean
     IRestaurantDAO restaurantDAO;
 
-
     @Resource
     SessionObject sessionObject;
 
     @Test
     public void registerTest() {
-        RegistrationModel registrationModel = new RegistrationModel();
-        registrationModel.setName("Adam");
-        registrationModel.setLastname("Nowak");
-        registrationModel.setLogin("adanow");
-        registrationModel.setEmail("adanow@mail.com");
-        registrationModel.setPass("adam");
-        registrationModel.setPass2("adam");
-
-        Mockito.when(this.userDAO.getUserByLogin("adanow")).thenReturn(null);
+        RegistrationModel registrationModel = new RegistrationModel("Adam", "Nowak", "adanow123", "adanow@email.com", "adanow123", "adanow123");
+        Mockito.when(this.userDAO.getUserByLogin("adanow123")).thenReturn(null);
         Mockito.when(this.userDAO.persistUser(ArgumentMatchers.any())).thenReturn(true);
-        boolean result = userService.register(registrationModel);
-        Assert.assertTrue(result);
+
+        boolean result = this.userService.register(registrationModel);
+
+        Assertions.assertTrue(result);
     }
+
+
+    @Test
+    public void loginTest() {
+        User user = new User(1, "Adam", "Nowak", "adanow123", "adanow@email.com", "adanow123", User.Role.USER);
+        Mockito.when(this.userDAO.getUserByLogin("adanow123")).thenReturn(new User(1, "Adam", "Nowak", "adanow123", "adanow@email.com", "adanow123", User.Role.USER));
+
+        this.userService.authenticate(user);
+
+        Assert.assertNotNull(this.sessionObject.getLoggedUser());
+
+    }
+
+    @Test
+    public void passwordTest() {
+        User user = new User(1, "Adam", "Nowak", "adanow123", "adanow@email.com", "adanow123", User.Role.USER);
+        Mockito.when(this.userDAO.getUserByLogin("tomek123")).thenReturn(new User(1, "Adam", "Nowak", "adanow123", "adanow@email.com", "adanow333", User.Role.USER));
+
+        this.userService.authenticate(user);
+
+        Assert.assertNull(this.sessionObject.getLoggedUser());
+    }
+
+
 
 }
